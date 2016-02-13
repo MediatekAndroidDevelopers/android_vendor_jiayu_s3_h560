65280a9c3e9206674859c6f69f2b0224b577ae7f
fire855 <thefire855@gmail.com>
2016-02-13 21:38:45 +0100

    Initial commit

---
+++ s3_h560-vendor-blobs.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/vendor,system/vendor)
