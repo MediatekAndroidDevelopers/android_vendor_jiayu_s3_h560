9ad2ad253e249abb9386853e240ecc9a3f696fcc
fire855 <thefire855@gmail.com>
2016-02-16 16:38:07 +0100

    Add more nfc files

---
+++ Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudiocustparam
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES_32 := lib/libaudiocustparam.so
+LOCAL_SRC_FILES_64 := lib64/libaudiocustparam.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+include $(BUILD_PREBUILT)
