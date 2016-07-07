a0e9bec0fec446fd96e29ddc42a17ce5e608577f
fire855 <thefire855@gmail.com>
2016-07-07 13:29:01 +0200

    Fix libhwm linking

--- Android.mk
+++ Android.mk
@@ -1,4 +1,7 @@
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),s3_h560)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhwm
 LOCAL_SRC_FILES_64 := lib64/libhwm.so
@@ -7,3 +10,5 @@ LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
+
+endif
