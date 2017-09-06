e47ae567103bf20b54c3b869ce9a177c378f3d90
fire855 <thefire855@gmail.com>
2017-09-06 22:26:42 +0200

    Move ramdisk services to vendor
    Conflicts:
    	h5000-vendor-blobs.mk
    	xbin/BGW

--- s3_h560-vendor-blobs.mk
+++ s3_h560-vendor-blobs.mk
@@ -8,6 +8,4 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/etc,s
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/xbin,system/xbin)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/vendor,system/vendor)
