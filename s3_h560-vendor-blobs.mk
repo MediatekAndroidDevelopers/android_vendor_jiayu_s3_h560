b24e27fb30df55a973795ce232573d4799ebbb41
DerTeufel <dominik-kassel@gmx.de>
2017-10-17 11:39:07 +0200

    nfcstackp remains in xbin

--- s3_h560-vendor-blobs.mk
+++ s3_h560-vendor-blobs.mk
@@ -1,3 +1,5 @@
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/usr,system/usr)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/xbin,system/xbin)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/vendor,system/vendor)
